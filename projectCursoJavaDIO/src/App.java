71b53f1608939450e3cc0ab0b822b43f091c8278
edsonbdc <edsonsbdc@gmail.com>
2023-10-23 00:21:37 -0300

    projetoJavaBasico

--- projectCursoJavaDIO/src/App.java
+++ projectCursoJavaDIO/src/App.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner teclado = new Scanner(System.in);
+        int option = 0;
+        String name = null, userName = null, password = null;
+        do {
+            System.out.println("============My Bank============");
+            System.out.println("1-new account | 2-list accounts | 3-Close");
+            option = teclado.nextInt();
+            teclado.nextLine();
+            switch (option) {
+                case 1:
+                    System.out.println("Name: ");
+                    name = teclado.nextLine();
+                    System.out.println("User name: ");
+                    userName = teclado.nextLine();
+                    System.out.println("Password: ");
+                    password = teclado.nextLine();
+                    break;
+                case 2:
+                    System.out.println("Name: "+name+"\n"+"User name: "+userName+"\n"+"Password: "+password);
+                    break;
+                case 3:
+                default:
+                    System.out.println("App closed");
+
+            }
+        } while (option != 3);
     }
 }
